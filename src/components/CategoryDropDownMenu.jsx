ea9ea2cc66c3deff2b1fbd961d6269dfdf3d4db5
Zoe X <zoexu02@gmail.com>
2024-11-11 12:25:37 -0800

    updates

--- src/components/CategoryDropDownMenu.jsx
+++ src/components/CategoryDropDownMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const CategoryDropDownMenu = ({ closeDropDown, setDropDownSelection }) => {
   // Function to send state back to addNewFormDisplay with selection and menu visibility status
@@ -22,36 +22,117 @@ const CategoryDropDownMenu = ({ closeDropDown, setDropDownSelection }) => {
             className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
             tabIndex='-1'
             id='healthAndFitness'
-            onClick={() => handleClick('Health and wellness')}
+            onClick={() => handleClick('Entertainment')}
           >
-            Health and wellness
+            Entertainment
           </a>
           <a
             href='#'
             className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
             tabIndex='-1'
             id='wellness'
-            onClick={() => handleClick('Streaming services')}
+            onClick={() => handleClick('Productivity')}
           >
-            Streaming services
+            Productivity
           </a>
           <a
             href='#'
             className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
             tabIndex='-1'
             id='hobbies'
-            onClick={() => handleClick('News and magazines')}
+            onClick={() => handleClick('Utilities')}
           >
-            News and magazines
+            Utilities
           </a>
           <a
             href='#'
             className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
             tabIndex='-1'
             id='social'
-            onClick={() => handleClick('Misc')}
+            onClick={() => handleClick('Gaming')}
           >
-            Misc.
+            Gaming
+          </a>
+          <a
+            href='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
+            tabIndex='-1'
+            id='social'
+            onClick={() => handleClick('Education')}
+          >
+            Education
+          </a>
+          <a
+            href='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
+            tabIndex='-1'
+            id='social'
+            onClick={() => handleClick('Health and fitness')}
+          >
+            Health and fitness
+          </a>
+          <a
+            href='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
+            tabIndex='-1'
+            id='social'
+            onClick={() => handleClick('News and media')}
+          >
+            News and media
+          </a>
+          <a
+            href='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
+            tabIndex='-1'
+            id='social'
+            onClick={() => handleClick('Shopping')}
+          >
+            Shopping
+          </a>
+          <a
+            href='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
+            tabIndex='-1'
+            id='social'
+            onClick={() => handleClick('Social media')}
+          >
+            Social media
+          </a>
+          <a
+            href='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
+            tabIndex='-1'
+            id='social'
+            onClick={() => handleClick('Security')}
+          >
+            Security
+          </a>
+          <a
+            href='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
+            tabIndex='-1'
+            id='social'
+            onClick={() => handleClick('Food delivery')}
+          >
+            Food delivery
+          </a>
+          <a
+            href='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
+            tabIndex='-1'
+            id='social'
+            onClick={() => handleClick('Storage')}
+          >
+            Storage
+          </a>
+          <a
+            href='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200'
+            tabIndex='-1'
+            id='social'
+            onClick={() => handleClick('Other')}
+          >
+            Other
           </a>
         </div>
       </div>
