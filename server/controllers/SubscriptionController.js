054beedef5b38644b97bdc134a8431234a4572dd
Julie Hoagland-Sorensen <143046484+JulieHoaglandSorensen@users.noreply.github.com>
2024-11-11 16:42:18 -0500

    Afte rNewForm update

--- server/controllers/SubscriptionController.js
+++ server/controllers/SubscriptionController.js
@@ -6,94 +6,94 @@ const subscriptionController = {};
 
 //create a new subscription for a given user
 subscriptionController.createSubscription = async (req, res, next) => {
-  const {
-    userId,
-    serviceName,
-    amount,
-    status,
-    billingCycle,
-    nextPaymentDate,
-    category,
-    notifyDaysBefore,
-  } = req.body;
+	const {
+		userId,
+		serviceName,
+		amount,
+		status,
+		billingCycle,
+		nextPaymentDate,
+		category,
+		notifyDaysBefore,
+	} = req.body;
 
-  try {
-    const newSub = await Subscriptions.create({
-      userId,
-      serviceName,
-      amount,
-      status,
-      billingCycle,
-      nextPaymentDate,
-      category,
-      notifyDaysBefore,
-    });
-    //will return the new subscription
-    res.locals.newSub = newSub;
-    return next();
-  } catch (err) {
-    return next({
-      log: "Error in 'createSubscription' middleware: " + err,
-      status: 500,
-      message: { err: 'An error occurred while creating a new subscription' },
-    });
-  }
+	try {
+		const newSub = await Subscription.create({
+			userId,
+			serviceName,
+			amount,
+			status,
+			billingCycle,
+			nextPaymentDate,
+			category,
+			notifyDaysBefore,
+		});
+		//will return the new subscription
+		res.locals.newSub = newSub;
+		return next();
+	} catch (err) {
+		return next({
+			log: "Error in 'createSubscription' middleware: " + err,
+			status: 500,
+			message: { err: 'An error occurred while creating a new subscription' },
+		});
+	}
 };
 
 //update existing subscription by subscription ID
 subscriptionController.updateSubscription = async (req, res, next) => {
-  const { _id } = req.params;
-  const {
-    serviceName,
-    amount,
-    status,
-    billingCycle,
-    nextPaymentDate,
-    category,
-    notifyDaysBefore,
-  } = req.body;
+	const { _id } = req.params;
+	const {
+		serviceName,
+		amount,
+		status,
+		billingCycle,
+		nextPaymentDate,
+		category,
+		notifyDaysBefore,
+	} = req.body;
 
-  try {
-    const updatedSub = await Subscriptions.findByIdAndUpdate(
-      _id,
-      {
-        serviceName,
-        amount,
-        status,
-        billingCycle,
-        nextPaymentDate,
-        category,
-        notifyDaysBefore,
-      },
-      { new: true }
-    );
-    //will return the updated subscription details
-    res.locals.updatedSub = updatedSub;
-    return next();
-  } catch (err) {
-    return next({
-      log: "Error in 'updateSubscription' middleware: " + err,
-      status: 500,
-      message: { err: 'An error occurred while updating subscription' },
-    });
-  }
+	try {
+		const updatedSub = await Subscription.findByIdAndUpdate(
+			_id,
+			{
+				serviceName,
+				amount,
+				status,
+				billingCycle,
+				nextPaymentDate,
+				category,
+				notifyDaysBefore,
+			},
+			{ new: true }
+		);
+		//will return the updated subscription details
+		res.locals.updatedSub = updatedSub;
+		return next();
+	} catch (err) {
+		return next({
+			log: "Error in 'updateSubscription' middleware: " + err,
+			status: 500,
+			message: { err: 'An error occurred while updating subscription' },
+		});
+	}
 };
 
 //delete specific subscription based on subscription ID
 subscriptionController.deleteSubscription = async (req, res, next) => {
-  const { _id } = req.params;
+	const { _id } = req.params;
 
-  try {
-    const deletedSub = Subscriptions.findByIdAndDelete(_id);
-    res.locals.deletedSub = deletedSub;
-    return next();
-  } catch (err) {
-    return next({
-      log: "Error in 'deleteSubscription' middleware: " + err,
-      status: 500,
-      message: { err: 'An error occurred while deleting subscription' },
-    });
-  }
+	try {
+		const deletedSub = Subscription.findByIdAndDelete(_id);
+		res.locals.deletedSub = deletedSub;
+		return next();
+	} catch (err) {
+		return next({
+			log: "Error in 'deleteSubscription' middleware: " + err,
+			status: 500,
+			message: { err: 'An error occurred while deleting subscription' },
+		});
+	}
 };
 
 export default subscriptionController;
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -8,71 +8,71 @@ const userController = {};
 
 //get a user and all their subscriptions/trials based on user ID
 userController.getUser = async (req, res, next) => {
-  //grab userID from auth redirect
-  const userID = req.params.id;
+	//grab userID from auth redirect
+	const userID = req.params.id;
 
-  try {
-    // const foundUser = await User.findById(_id, 'name email');
-    // if (!user) return next ({
-    //   log: 'findOne query returned null in getUser - email not found in DB',
-    //   status: 404,
-    //   message: {
-    //     err: `No user found in database with email ${foundUser}`
-    //   }
-    // });
-    // res.locals.foundUser = foundUser;
-    // return next();
+	try {
+		// const foundUser = await User.findById(_id, 'name email');
+		// if (!user) return next ({
+		//   log: 'findOne query returned null in getUser - email not found in DB',
+		//   status: 404,
+		//   message: {
+		//     err: `No user found in database with email ${foundUser}`
+		//   }
+		// });
+		// res.locals.foundUser = foundUser;
+		// return next();
 
-    await User.findOne({ _id: ObjectId(userID) }).then((result) => {
-      //if no matching user was found
-      if (result === null) {
-        return next({
-          log: 'findOne query returned null in getUser - email not found in DB',
-          status: 404,
-          message: {
-            err: `No user found in database with email ${userID}`,
-          },
-        });
-      }
-      res.locals.foundUser = result;
-    });
-  } catch (error) {
-    return next({
-      log: error,
-      status: 500,
-      message: { err: 'User was unable to be retrieved from the database.' },
-    });
-  }
+		await User.findOne({ _id: userID }).then((result) => {
+			//if no matching user was found
+			if (result === null) {
+				return next({
+					log: 'findOne query returned null in getUser - email not found in DB',
+					status: 404,
+					message: {
+						err: `No user found in database with email ${userID}`,
+					},
+				});
+			}
+			res.locals.foundUser = result;
+		});
+	} catch (error) {
+		return next({
+			log: error,
+			status: 500,
+			message: { err: 'User was unable to be retrieved from the database.' },
+		});
+	}
 
-  //get all of users' subscriptions
-  try {
-    await Subscription.find({ userId: ObjectId(userID) }).then(
-      (result) => (res.locals.subscriptions = result)
-    );
-  } catch (error) {
-    return next({
-      log: error,
-      status: 500,
-      message: {
-        err: "User's subscriptions were unable to be retrieved from the database.",
-      },
-    });
-  }
+	//get all of users' subscriptions
+	try {
+		await Subscription.find({ userId: userID }).then(
+			(result) => (res.locals.subscriptions = result)
+		);
+	} catch (error) {
+		return next({
+			log: error,
+			status: 500,
+			message: {
+				err: "User's subscriptions were unable to be retrieved from the database.",
+			},
+		});
+	}
 
-  //get all of users' trials
-  try {
-    const trials = await Trial.find({ userId: ObjectId(userID) });
-    res.locals.trials = trials;
-    return next();
-  } catch (error) {
-    return next({
-      log: error,
-      status: 500,
-      message: {
-        err: "User's trials were unable to be retrieved from the database.",
-      },
-    });
-  }
+	//get all of users' trials
+	try {
+		const trials = await Trial.find({ userId: userID });
+		res.locals.trials = trials;
+		return next();
+	} catch (error) {
+		return next({
+			log: error,
+			status: 500,
+			message: {
+				err: "User's trials were unable to be retrieved from the database.",
+			},
+		});
+	}
 };
 
 export default userController;
