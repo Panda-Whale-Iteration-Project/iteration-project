254a66c1672de330903d1c0d22ae0f2359bc5c80
claireandj666 <claire.yandi.chen@gmail.com>
2024-11-14 08:07:48 -0800

    add trial display

--- src/components/TrialDisplayRearrange.jsx
+++ src/components/TrialDisplayRearrange.jsx
@@ -11,21 +11,59 @@ const TrialDisplayRearrange = ({ userData }) => {
 			try {
 				const response = await fetch(
 					`http://localhost:3000/user/${userData.subscriptionUser._id}`
-				)
-			} catch (error) {
+				);
 
+				if (!response.ok) {
+					throw new Error('Unable to fetch data');
+				}
+
+				const data = await response.json();
+				setTrialData(data.trials);
+
+			} catch (error) {
+				setError(error.message);
 			}
 		}
-	})
+		fetchTrialsData();
+	}, [])
+
+	const handleEditClick = (id) => {
+    // Logic for handling edit click
+    console.log("Edit subscription with ID:", id);
+  };
+
+  const handleDeleteClick = (id) => {
+    // Logic for handling delete click
+    console.log("Delete subscription with ID:", id);
+  };
 
 	const columns = [
     { field: 'trialName', headerName: 'Service Name', width: 120},
     { field: 'category', headerName: 'Category', width: 120 },
+		{ field: 'amount', headerName: 'Amount ($)', width: 120 },
+		{ field: 'expDate', headerName: 'Expire Date', width: 120 },
+		{ field: 'category', headerName: 'Category', width: 120 },
+		{ field: 'detail', headerName: 'More details', width: 120 },
+		{ field: 'action', headerName: 'Action', width: 120,
+			rederCell: (params) => (
+				<div>
+					<Button color="primary" onClick={() => handleEditClick(params.id)}>
+						Edit
+					</Button>
+					<Button color="secondary" onClick={() => handleDeleteClick(params.id)}>
+						Delete
+					</Button>
+				</div>
+			),
+		},
 	]
 
 	return (
 		<div style={{ height: 400, width: '100%' }}>
-
+			<DataGrid 
+				rows={trialData}
+				columns={columns}
+			/>
 		</div>
 	)
 }
